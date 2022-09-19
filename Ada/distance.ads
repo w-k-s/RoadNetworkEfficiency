7cdb0b1b0e6b3dd9c075eff16c00339aee56458f
w-k-s <waqqas.abdulkareem@gmail.com>
2022-09-19 21:53:32 +0400

    Remove ada implementation

--- Ada/distance.ads
+++
@@ -1,9 +0,0 @@
-with Geo; use Geo;
-
-package Distance is
-
-type Km is new Float;
-
-function CityBlockDistance (LatLng1 : LatLng; LatLng2 : LatLng) return Km;
-
-end Distance;
--- Ada/geo.ads
+++
@@ -1,8 +0,0 @@
-package Geo is
-
-type LatLng is record
-   Lat : Float;
-   Lng: Float;
-end record;
-
-end Geo;
